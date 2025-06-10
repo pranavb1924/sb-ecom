80c24716bce6b06d6c75706ef4f3d3c2a9c9060b
Pranav B <bhoopal.p@northeastern.edu>
2025-06-10 13:45:29 -0400

    Added new category services

---
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.project.service;
+import com.ecommerce.project.model.Category;
+import org.springframework.http.ResponseEntity;
+
+import java.util.*;
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+    String deleteCategory(Long categoryId);
+    String updateCategory(Category category);
+}
